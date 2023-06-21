a247458bc113033e1a722fed9d614331065bb02d
Viktoriia Chumak <vikaff17@gmail.com>
2023-06-21 11:39:24 +0300

    Updates

---
+++ static/js/270.5226e354.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbubu_project=self.webpackChunkbubu_project||[]).push([[270],{270:function(u,e,c){c.r(e);e.default=function(){}}}]);
+//# sourceMappingURL=270.5226e354.chunk.js.map
---
+++ static/js/664.a30a8b9d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbubu_project=self.webpackChunkbubu_project||[]).push([[664],{664:function(u,e,c){c.r(e);e.default=function(){}}}]);
+//# sourceMappingURL=664.a30a8b9d.chunk.js.map
